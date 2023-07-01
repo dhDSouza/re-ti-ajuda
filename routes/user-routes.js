6ea6e147038f1e059d0de7f9a9f149966955d6d6
Daniel Henrique de Souza <dhs.danielsouza@gmail.com>
2023-07-01 18:12:41 -0300

    Create user routes

---
+++ routes/user-routes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router()
+const userController = require('../controllers/user-controller')
+const auth = require('../middlewares/auth-middleware')
+const { userAlredyExist } = require('../middlewares/validation-middleware')
+
+router.post('/register', userAlredyExist, userController.registerUser)
+router.post('/login', userController.loginUser)
+router.put('/:id', auth, userController.updateUser)
+router.delete('/:id', auth, userController.deleteUser)
+
+module.exports = router
