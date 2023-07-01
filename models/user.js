6548a29d9454539dd7cd8232edbafffa40b664c2
Daniel Henrique de Souza <dhs.danielsouza@gmail.com>
2023-07-01 13:04:28 -0300

    Fixed bugs with user model and repository

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize')
-const sequelize = require('../config/database')
+const sequelize = require('../config/connection')
 const User = sequelize.define('User', {
 	id: {
 		type: DataTypes.INTEGER,
--- repositories/user-repository.js
+++ repositories/user-repository.js
@@ -1,4 +1,4 @@
-const User = require('../models/User')
+const User = require('../models/user')
 
 async function createUser(username, password) {
 	return User.create({ username, password })
