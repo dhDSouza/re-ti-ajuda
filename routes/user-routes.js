47e41359859aa452f06bbd1d59a3ecea87bc5c64
Daniel Henrique de Souza <dhs.danielsouza@gmail.com>
2023-07-01 19:59:32 -0300

    Fixed bug in validation middleware call

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,9 +1,9 @@
 const router = require('express').Router()
 const userController = require('../controllers/user-controller')
 const auth = require('../middlewares/auth-middleware')
-const { userAlredyExist } = require('../middlewares/validation-middleware')
+const { userAlreadyExist } = require('../middlewares/validation-middleware')
 
-router.post('/register', userAlredyExist, userController.registerUser)
+router.post('/register', userAlreadyExist, userController.registerUser)
 router.post('/login', userController.loginUser)
 router.post('/logout', auth, userController.logoutUser)
 router.put('/:id', auth, userController.updateUser)
