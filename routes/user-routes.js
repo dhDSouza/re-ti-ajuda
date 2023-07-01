92e210e3475ffffe78329abd0c80227802c5d5c5
Daniel Henrique de Souza <dhs.danielsouza@gmail.com>
2023-07-01 19:15:39 -0300

    Add logout route

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -5,6 +5,7 @@ const { userAlredyExist } = require('../middlewares/validation-middleware')
 
 router.post('/register', userAlredyExist, userController.registerUser)
 router.post('/login', userController.loginUser)
+router.post('/logout', auth, userController.logoutUser)
 router.put('/:id', auth, userController.updateUser)
 router.delete('/:id', auth, userController.deleteUser)
 
