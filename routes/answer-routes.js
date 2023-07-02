498790489c7a401f5dbeb8625ff6205d2e34e4e8
Daniel Henrique de Souza <dhs.danielsouza@gmail.com>
2023-07-02 16:12:19 -0300

    Refactor auth middleware and routes

--- routes/answer-routes.js
+++ routes/answer-routes.js
@@ -1,10 +1,10 @@
 const router = require('express').Router()
 const answerController = require('../controllers/answer-controller')
 const auth = require('../middlewares/auth-middleware')
-const { answerNotFound } = require('../middlewares/validation-middleware')
+const { questionNotFound, answerNotFound } = require('../middlewares/validation-middleware')
 
-router.get('/:id', auth, answerNotFound, answerController.findAnswerByQuestionId)
-router.post('/', auth, answerController.createAnswer)
+router.get('/:id', auth, questionNotFound, answerController.findAnswerByQuestionId)
+router.post('/', auth, questionNotFound, answerController.createAnswer)
 router.put('/:id', auth, answerNotFound, answerController.updateAnswer)
 router.delete('/:id', auth, answerNotFound, answerController.deleteAnswer)
 
