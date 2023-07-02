2be9cffabe0140c628028d3c9b8f49f68e44b6c0
Daniel Henrique de Souza <dhs.danielsouza@gmail.com>
2023-07-02 16:00:02 -0300

    Refactor the answer controller functions.

--- controllers/answer-controller.js
+++ controllers/answer-controller.js
@@ -6,7 +6,7 @@ async function createAnswer(req, res) {
 	const { description } = req.body
 
 	try {
-		await AnswerService.createAnswer(questionId, userId, description)
+		await AnswerService.createAnswer(userId, questionId, description)
 		res.status(201).json({ message: 'Resposta criada com sucesso' })
 	} catch (error) {
 		res.status(500).json({ message: 'Erro ao criar resposta', error: error.message })
@@ -25,11 +25,11 @@ async function findAnswerByQuestionId(req, res) {
 }
 
 async function updateAnswer(req, res) {
-	const { answerId } = req.params
+	const { id } = req.params
 	const { description } = req.body
 
 	try {
-		await AnswerService.updateAnswer(answerId, description)
+		await AnswerService.updateAnswer(id, description)
 		res.status(200).json({ message: 'Resposta atualizada com sucesso' })
 	} catch (error) {
 		res.status(500).json({ message: 'Erro ao atualizar resposta', error: error.message })
@@ -37,10 +37,10 @@ async function updateAnswer(req, res) {
 }
 
 async function deleteAnswer(req, res) {
-	const { answerId } = req.params
+	const { id } = req.params
 
 	try {
-		await AnswerService.deleteAnswer(answerId)
+		await AnswerService.deleteAnswer(id)
 		res.status(200).json({ message: 'Resposta deletada com sucesso' })
 	} catch (error) {
 		res.status(500).json({ message: 'Erro ao deletar resposta', error: error.message })
